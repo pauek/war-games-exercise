b25422d247163b9207fabaa179258bd80adafd63
Pau Fernández <pau.fernandez@upc.edu>
2019-11-11 18:34:25 +0100

    Añado enunciado

---
+++ war-games-exercise.js
@@ -0,0 +1,17 @@
+
+/* 
+
+Escribe un programa que muestra las letras de:
+
+  You should know that professor.
+  You programmed me.
+
+con una diferencia de 100 milisegundos entre letra y letra.
+Para los saltos de línea, añade 700 milisegundos extra.
+
+*/
+
+const texto = `
+You should know that, professor.
+You programmed me.
+`
